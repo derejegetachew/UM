53d538e06bb9dc1e65432d4aa64c83da7cad7521
Dereje Getachew <101693929+derejegetachew@users.noreply.github.com>
2024-09-25 10:48:48 +0300

    add database

---
+++ configration/dbconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "10.1.50.108",
+    USER: "derejeg",
+    PASSWORD: "123456",
+    DB: "userm",
+    dialect: "mysql", 
+    pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+    }
+    };
